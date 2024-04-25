52e9069776e61a1a451bca0777aea88c0b2a034c
root <alexnewman42@gmail.com>
2024-04-25 08:56:16 -0400

    Add code to check for palindromes

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ const result = document.getElementById("result");
 checkBtn.addEventListener("click", () => {
     if (textInput.value === "") {
         alert("Please input a value");
+    } else if (textInput.value.length === 1) {
+        result.innerText = `${textInput.value} is a palindrome`;
+    } else if (textInput.value === [...textInput.value].reverse().join("")) {
+        result.innerText = `${textInput.value} is a palindrome`;
     }
 });
