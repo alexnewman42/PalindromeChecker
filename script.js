fde21f35cebeb7771f243ca2ec47a0da38ec6129
root <alexnewman42@gmail.com>
2024-04-26 07:30:31 -0400

    Add regex to filter out non-standard characters

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const textInput = document.getElementById("text-input");
 const result = document.getElementById("result");
 
 checkBtn.addEventListener("click", () => {
+    const replaced = textInput.value.replace(/[^A-Za-z0-9]/g,"");
     if (textInput.value === "") {
         alert("Please input a value");
+
     } else if (textInput.value.length === 1) {
         result.innerText = `${textInput.value} is a palindrome`;
-    } else if (textInput.value === [...textInput.value].reverse().join("")) {
+
+    } else if (replaced === [...replaced].reverse().join("")) {
         result.innerText = `${textInput.value} is a palindrome`;
     }
 });
