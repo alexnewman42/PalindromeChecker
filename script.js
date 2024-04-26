fca47256caf5b667b44dfede7d52e0fc1be8ba84
root <alexnewman42@gmail.com>
2024-04-26 07:42:42 -0400

    Added negative result else statement, tested and finished

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const textInput = document.getElementById("text-input");
 const result = document.getElementById("result");
 
 checkBtn.addEventListener("click", () => {
-    const replaced = textInput.value.replace(/[^A-Za-z0-9]/g,"");
+    const lowerReplaced = textInput.value.toLowerCase().replace(/[^a-z0-9]/g,"");
+
     if (textInput.value === "") {
         alert("Please input a value");
 
     } else if (textInput.value.length === 1) {
         result.innerText = `${textInput.value} is a palindrome`;
 
-    } else if (replaced === [...replaced].reverse().join("")) {
+    } else if (lowerReplaced === [...lowerReplaced].reverse().join("")) {
         result.innerText = `${textInput.value} is a palindrome`;
+    } else {
+        result.innerText =  `${textInput.value} is not a palindrome`;
     }
 });
